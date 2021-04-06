18ba353717806af6903c17d687b3bbf9b6017ace
f73chen <grace.fenglin@gmail.com>
2021-04-06 18:44:38 -0400

    Javascript

---
+++ udemy-bootcamp/JavaScript/first.js
@@ -0,0 +1,28 @@
+// Use console.log() to print to console
+console.log("HELLO WORLD");
+
+// Arrays can contain any combination of values
+// days[2][0] = 'M'
+let days = [68, true, 'Monday', NaN]
+
+// Becomes [68, true, 'Monday', NaN, empty x 6, 'indigo]
+// Length becomes 11
+days[10] = 'indigo'
+
+// .push() --> add to end
+    // Can push multiple csv
+// .pop() --> remove from end
+// .shift() --> remove from start
+// .unshift() --> add to start
+
+// .concat() --> merge arrays
+// .includes() --> look for a value
+// .indexOf()
+// .join() --> creates a string from an array
+// .reverse() --> reverses an array
+// .slice() --> copies a portion of an array
+// .splice() --> removes/replaces elements in-place
+// .sort() --> sorts an array
+
+// Arrays only equal if they point to the same memory address
+// Can be stored as a const
